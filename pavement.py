a8e676b4585209175e983b3d369f6e331578b28c
Ted Lilley <ted.lilley@gmail.com>
2012-10-24 01:13:17 -0400

    Add posts, pages, s3sysnc.#	media/statistics.png

---
+++ pavement.py
@@ -0,0 +1,94 @@
+import subprocess as sub
+from paver.easy import *
+import yaml
+from unipath import Path
+from bunch import *
+
+def bunchify_yaml_file(filename):
+    '''
+    Load and bunchify a yaml file
+
+    '''
+    yaml_data = yaml.safe_load(Path(filename).read_file())
+    result = bunchify(yaml_data)
+    return result
+
+
+@task
+@cmdopts([('opts=', 'o', 'an options file')])
+def auto(options):
+    '''
+    Load my task options
+
+    '''
+    this_file = Path(__file__)
+    cfg_file = this_file.absolute().parent.child('pavement_config.yml')
+    cfg = bunchify_yaml_file(cfg_file)
+    options.cfg = cfg
+
+
+@task
+def clean(options):
+    '''
+    Clean the last build
+
+    '''
+    cfg = options.cfg.default
+    clean_dirs = [_dir % cfg for _dir in cfg.clean_dirs]
+    clean_dirs = [Path(_dir).absolute() for _dir in clean_dirs if _dir and Path.isdir(_dir)]
+    for _dir in clean_dirs:
+        if _dir.components() < 4:
+            print 'Directory %s is too close to the root.  Skipping.' % _dir
+            continue
+        cmd = 'rd /s /q %(_dir)s' % locals()
+        sub.check_call(cmd, shell=True)
+
+
+@task
+def run(options):
+    '''
+    Run the development server.
+
+    '''
+    cmd = 'rackup.bat -p 3000'
+    sub.check_call(cmd)
+
+
+@task
+def build(options):
+    '''
+    Build the distribution.
+
+    '''
+    cfg = options.cfg.default
+    cmd = 'git commit > nul'
+    print 'Committing...'
+    sub.call(cmd, shell=True)
+    cmd = 'git push > nul'
+    print 'Pushing...'
+    sub.check_call(cmd, shell=True)
+    cmd = 'ruhoh.bat compile > nul'
+    print 'Compiling...'
+    sub.check_call(cmd, shell=True)
+    cmd = r'xcopy /y /s %(contrib_dir)s %(build_dir)s > nul' % cfg
+    print 'Copying contrib files...'
+    sub.check_call(cmd, shell=True)
+    print 'Done.'
+
+
+@task
+@needs(['build'])
+def release(options):
+    '''
+    Release the distributable to ftp
+    
+    '''
+    cmd = 's3sync'
+    print 'Syncing to S3...'
+    sub.check_call(cmd)
+    print 'Done'
+
+
+if __name__ == '__main__':
+    import paver.tasks
+    paver.tasks.main()
---
+++ posts/inverse-minimak-pkl-mapping-fixed.md
@@ -0,0 +1,18 @@
+---
+title: Inverse Minimak PKL mapping fixed
+date: '2012-10-22'
+description:
+categories: revisions
+---
+
+I noticed that the portable key layout mapping for Minimak-to-QWERTY had
+the "M" position typing "R".
+
+I've posted a new revision to github, which you can [download here].  I
+posted it around 5pm today.
+
+Sorry for the inconvenience, and if you find any problems with the
+layouts, submit [an issue] on github.
+
+[download here]: https://github.com/downloads/lilleyt/minimak/minimak.zip
+[an issue]: https://github.com/lilleyt/minimak/issues/new
